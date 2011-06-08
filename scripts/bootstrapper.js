b20dd48d783df70645e28832624a2f1f62ed537f
James Randall <james@accidentalfish.com>
2011-06-08 19:48:32 +0100

    Fixed issue with feature detection

--- scripts/bootstrapper.js
+++ scripts/bootstrapper.js
@@ -2,20 +2,21 @@ require(["jquery",
         "metrics",
 		"jsExtensions",
 		"astar"], function($, metrics, jsExtensions, astar) {
-	if (Modernizr.canvastext)
-	{
-		jsExtensions();
 	
-	    $(document).ready(function() {	
+	jsExtensions();
+
+    $(document).ready(function() {	
+		if (Modernizr.canvastext)
+		{
 			$("#astar").append(
 				$('<canvas id="astarCanvas" width="' + $("#astar").width() + '" height="' + $("#astar").height() + '">')
 			);
 			astar.begin();
-	    });
-	}
-	else
-	{
-		$("#browserNotSupported").show();
-		$("#astar").hide();
-	}
+		}
+		else
+		{
+			$("#browserNotSupported").show();
+			$("#astar").hide();
+		}
+    });
 });
